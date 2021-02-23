4c8d53c7bf96a33c44c7d142592bd5a653ecb273
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 18:14:56 -0500

    Add enum logic to transactions.result, add has_many relationships through invoices

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,10 @@
 class Customer < ApplicationRecord
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+  has_many :items, through: :invoices
 
+  def self.all_successful_transactions_count
+    Customer.joins(:transactions).where("transactions.result = ?", 0).select("customers.*, count('transactions') as successful_transactions_count").group(:id)
+  end
 end
