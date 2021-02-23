87c4abf1b1a41dffae0d51b9a5ae45e0e48d7689
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 16:33:13 -0500

    Complete feature test setup and test for User Story 3 - Top Customers

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,23 +1,110 @@
 require 'rails_helper'
 
 RSpec.describe "Admin Dashboard" do
+  before :each do
+    @customer_1 = create(:customer)
+
+    @invoice_1 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_2 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_3 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_4 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_5 = create(:invoice, customer_id: @customer_1.id)
+
+    @transaction_1 = create(:transaction, result: "success", invoice_id: @invoice_1.id)
+    @transaction_2 = create(:transaction, result: "success", invoice_id: @invoice_2.id)
+    @transaction_3 = create(:transaction, result: "success", invoice_id: @invoice_3.id)
+    @transaction_4 = create(:transaction, result: "success", invoice_id: @invoice_4.id)
+    @transaction_5 = create(:transaction, result: "success", invoice_id: @invoice_5.id)
+
+    @customer_2 = create(:customer)
+    @invoice_21 = create(:invoice, customer_id: @customer_2.id)
+    @invoice_22 = create(:invoice, customer_id: @customer_2.id)
+    @invoice_23 = create(:invoice, customer_id: @customer_2.id)
+    @invoice_24 = create(:invoice, customer_id: @customer_2.id)
+    @invoice_25 = create(:invoice, customer_id: @customer_2.id)
+
+    @transaction_21 = create(:transaction, result: "failed", invoice_id: @invoice_21.id)
+    @transaction_22 = create(:transaction, result: "failed", invoice_id: @invoice_22.id)
+    @transaction_23 = create(:transaction, result: "failed", invoice_id: @invoice_23.id)
+    @transaction_24 = create(:transaction, result: "success", invoice_id: @invoice_24.id)
+    @transaction_25 = create(:transaction, result: "failed", invoice_id: @invoice_25.id)
+
+    @customer_3 = create(:customer)
+    @invoice_31 = create(:invoice, customer_id: @customer_3.id)
+    @invoice_32 = create(:invoice, customer_id: @customer_3.id)
+    @invoice_33 = create(:invoice, customer_id: @customer_3.id)
+    @invoice_34 = create(:invoice, customer_id: @customer_3.id)
+    @invoice_35 = create(:invoice, customer_id: @customer_3.id)
+
+    @transaction_31 = create(:transaction, result: "failed", invoice_id: @invoice_31.id)
+    @transaction_32 = create(:transaction, result: "success", invoice_id: @invoice_32.id)
+    @transaction_33 = create(:transaction, result: "success", invoice_id: @invoice_33.id)
+    @transaction_34 = create(:transaction, result: "failed", invoice_id: @invoice_34.id)
+    @transaction_35 = create(:transaction, result: "failed", invoice_id: @invoice_35.id)
+
+    @customer_4 = create(:customer)
+    @invoice_41 = create(:invoice, customer_id: @customer_4.id)
+    @invoice_42 = create(:invoice, customer_id: @customer_4.id)
+    @invoice_43 = create(:invoice, customer_id: @customer_4.id)
+    @invoice_44 = create(:invoice, customer_id: @customer_4.id)
+    @invoice_45 = create(:invoice, customer_id: @customer_4.id)
+
+    @transaction_41 = create(:transaction, result: "success", invoice_id: @invoice_41.id)
+    @transaction_42 = create(:transaction, result: "success", invoice_id: @invoice_42.id)
+    @transaction_43 = create(:transaction, result: "success", invoice_id: @invoice_43.id)
+    @transaction_44 = create(:transaction, result: "failed", invoice_id: @invoice_44.id)
+    @transaction_45 = create(:transaction, result: "failed", invoice_id: @invoice_45.id)
+
+    @customer_5 = create(:customer)
+    @invoice_51 = create(:invoice, customer_id: @customer_5.id)
+    @invoice_52 = create(:invoice, customer_id: @customer_5.id)
+    @invoice_53 = create(:invoice, customer_id: @customer_5.id)
+    @invoice_54 = create(:invoice, customer_id: @customer_5.id)
+    @invoice_55 = create(:invoice, customer_id: @customer_5.id)
+
+    @transaction_51 = create(:transaction, result: "success", invoice_id: @invoice_51.id)
+    @transaction_52 = create(:transaction, result: "success", invoice_id: @invoice_52.id)
+    @transaction_53 = create(:transaction, result: "success", invoice_id: @invoice_53.id)
+    @transaction_54 = create(:transaction, result: "success", invoice_id: @invoice_54.id)
+    @transaction_55 = create(:transaction, result: "failed", invoice_id: @invoice_55.id)
+
+    customer_6 = create(:customer)
+    customer_7 = create(:customer)
+    customer_8 = create(:customer)
+    customer_9 = create(:customer)
+    customer_10 = create(:customer)
+  end
   describe "displays basic information" do
     it "should have a header indicating it is the Admin Dashboard" do
-      visit "/admin"
+      visit admin_index_path
 
       expect(page).to have_content("Admin Dashboard")
     end
 
     xit "should have links to the Admin merchant and invoice index" do
-      visit "/admin"
+      visit admin_index_path
 
-      click_link("Merchants")
+      click_button("Merchants")
       expect(current_path).to eq("/admin/merchants")
-      visit "/admin"
+      visit admin_index_path
 
-      click_link("Invoices")
+      click_button("Invoices")
       expect(current_path).to eq("/admin/invoices")
 
+    end
+  end
+  describe "overall admin level statistics" do
+    describe "top customer" do
+      it "should list the top 5 customers (largest no. of successful transactions)" do
+        visit admin_index_path
+
+        within ".navigation#merchants" do
+          expect(@customer_1).to appear_before(@customer_5)
+          expect(@customer_5).to appear_before(@customer_4)
+          expect(@customer_4).to appear_before(@customer_3)
+          expect(@customer_3).to appear_before(@customer_2)
+        end
+      end
     end
   end
 end
