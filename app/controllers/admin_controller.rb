ce6e0e0ee73a65bb63bca64d33284e495e3c5935
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 18:16:41 -0500

    Remove unused instance variables in index method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ApplicationController
 
   def index
-    @merchants = Merchant.all
-    @invoices = Invoice.all
     @top_customers = Customer.all_successful_transactions_count.order('successful_transactions_count desc').limit(5)
   end
 end
