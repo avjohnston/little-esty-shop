c064271b29641ccbbecf38cf039cfa6763dcd9c3
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 18:12:29 -0500

    Revise test to manually compare customers order with body.index method

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Admin Dashboard" do
   before :each do
-    @customer_1 = create(:customer)
+    @customer_1 = create(:customer, first_name: "Ace")
 
     @invoice_1 = create(:invoice, customer_id: @customer_1.id)
     @invoice_2 = create(:invoice, customer_id: @customer_1.id)
@@ -16,7 +16,7 @@ RSpec.describe "Admin Dashboard" do
     @transaction_4 = create(:transaction, result: "success", invoice_id: @invoice_4.id)
     @transaction_5 = create(:transaction, result: "success", invoice_id: @invoice_5.id)
 
-    @customer_2 = create(:customer)
+    @customer_2 = create(:customer, first_name: "Eli")
     @invoice_21 = create(:invoice, customer_id: @customer_2.id)
     @invoice_22 = create(:invoice, customer_id: @customer_2.id)
     @invoice_23 = create(:invoice, customer_id: @customer_2.id)
@@ -29,7 +29,7 @@ RSpec.describe "Admin Dashboard" do
     @transaction_24 = create(:transaction, result: "success", invoice_id: @invoice_24.id)
     @transaction_25 = create(:transaction, result: "failed", invoice_id: @invoice_25.id)
 
-    @customer_3 = create(:customer)
+    @customer_3 = create(:customer, first_name: "Danny")
     @invoice_31 = create(:invoice, customer_id: @customer_3.id)
     @invoice_32 = create(:invoice, customer_id: @customer_3.id)
     @invoice_33 = create(:invoice, customer_id: @customer_3.id)
@@ -42,7 +42,7 @@ RSpec.describe "Admin Dashboard" do
     @transaction_34 = create(:transaction, result: "failed", invoice_id: @invoice_34.id)
     @transaction_35 = create(:transaction, result: "failed", invoice_id: @invoice_35.id)
 
-    @customer_4 = create(:customer)
+    @customer_4 = create(:customer, first_name: "Cate")
     @invoice_41 = create(:invoice, customer_id: @customer_4.id)
     @invoice_42 = create(:invoice, customer_id: @customer_4.id)
     @invoice_43 = create(:invoice, customer_id: @customer_4.id)
@@ -55,7 +55,7 @@ RSpec.describe "Admin Dashboard" do
     @transaction_44 = create(:transaction, result: "failed", invoice_id: @invoice_44.id)
     @transaction_45 = create(:transaction, result: "failed", invoice_id: @invoice_45.id)
 
-    @customer_5 = create(:customer)
+    @customer_5 = create(:customer, first_name: "Bob")
     @invoice_51 = create(:invoice, customer_id: @customer_5.id)
     @invoice_52 = create(:invoice, customer_id: @customer_5.id)
     @invoice_53 = create(:invoice, customer_id: @customer_5.id)
@@ -97,13 +97,30 @@ RSpec.describe "Admin Dashboard" do
     describe "top customer" do
       it "should list the top 5 customers (largest no. of successful transactions)" do
         visit admin_index_path
+        save_and_open_page
 
-        within ".navigation#merchants" do
-          expect(@customer_1).to appear_before(@customer_5)
-          expect(@customer_5).to appear_before(@customer_4)
-          expect(@customer_4).to appear_before(@customer_3)
-          expect(@customer_3).to appear_before(@customer_2)
-        end
+        cust1 = page.body.index(@customer_1.first_name)
+        cust2 = page.body.index(@customer_2.first_name)
+        cust3 = page.body.index(@customer_3.first_name)
+        cust4 = page.body.index(@customer_4.first_name)
+        cust5 = page.body.index(@customer_5.first_name)
+
+        # within ".navigation#merchants" do
+        #   expect("#{@customer_1.first_name} #{@customer_1.last_name} - 5 purchases").to appear_before("#{@customer_5.first_name}", only_text: true)
+        #   expect("#{@customer_5.first_name}").to appear_before("#{@customer_4.first_name}", only_text: true)
+        #   expect("#{@customer_4.first_name}").to appear_before("#{@customer_3.first_name}", only_text: true)
+        #   expect("#{@customer_3.first_name}").to appear_before("#{@customer_2.first_name}", only_text: true)
+        # end
+        expect(cust1).to be < cust5
+        expect(cust1).to be < cust4
+        expect(cust1).to be < cust3
+        expect(cust1).to be < cust2
+        expect(cust5).to be < cust4
+        expect(cust5).to be < cust3
+        expect(cust5).to be < cust2
+        expect(cust4).to be < cust3
+        expect(cust4).to be < cust2
+        expect(cust3).to be < cust2
       end
     end
   end
