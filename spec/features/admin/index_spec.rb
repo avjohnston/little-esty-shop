504849d5c6e55391caf3c60ebebee500bfc56628
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 21:11:01 -0500

    Revise enumberable factory objects to be assigned with integers rather than strings

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -10,17 +10,17 @@ RSpec.describe "Admin Dashboard" do
     @invoice_4 = create(:invoice, customer_id: @customer_1.id)
     @invoice_5 = create(:invoice, customer_id: @customer_1.id)
 
-    @transaction_1 = create(:transaction, result: "success", invoice_id: @invoice_1.id)
-    @transaction_2 = create(:transaction, result: "success", invoice_id: @invoice_2.id)
-    @transaction_3 = create(:transaction, result: "success", invoice_id: @invoice_3.id)
-    @transaction_4 = create(:transaction, result: "success", invoice_id: @invoice_4.id)
-    @transaction_5 = create(:transaction, result: "success", invoice_id: @invoice_5.id)
+    @transaction_1 = create(:transaction, result: 0, invoice_id: @invoice_1.id)
+    @transaction_2 = create(:transaction, result: 0, invoice_id: @invoice_2.id)
+    @transaction_3 = create(:transaction, result: 0, invoice_id: @invoice_3.id)
+    @transaction_4 = create(:transaction, result: 0, invoice_id: @invoice_4.id)
+    @transaction_5 = create(:transaction, result: 0, invoice_id: @invoice_5.id)
 
-    ii_1 = create(:invoice_item, invoice_id: @invoice_1.id, status: "packaged")
-    ii_2 = create(:invoice_item, invoice_id: @invoice_1.id, status: "packaged")
-    ii_3 = create(:invoice_item, invoice_id: @invoice_2.id, status: "shipped")
-    ii_4 = create(:invoice_item, invoice_id: @invoice_2.id, status: "shipped")
-    ii_5 = create(:invoice_item, invoice_id: @invoice_3.id, status: "packaged")
+    ii_1 = create(:invoice_item, invoice_id: @invoice_1.id, status: 1)
+    ii_2 = create(:invoice_item, invoice_id: @invoice_1.id, status: 1)
+    ii_3 = create(:invoice_item, invoice_id: @invoice_2.id, status: 2)
+    ii_4 = create(:invoice_item, invoice_id: @invoice_2.id, status: 2)
+    ii_5 = create(:invoice_item, invoice_id: @invoice_3.id, status: 1)
 
 
     @customer_2 = create(:customer, first_name: "Eli")
@@ -30,13 +30,13 @@ RSpec.describe "Admin Dashboard" do
     @invoice_24 = create(:invoice, customer_id: @customer_2.id)
     @invoice_25 = create(:invoice, customer_id: @customer_2.id)
 
-    @transaction_21 = create(:transaction, result: "failed", invoice_id: @invoice_21.id)
-    @transaction_22 = create(:transaction, result: "failed", invoice_id: @invoice_22.id)
-    @transaction_23 = create(:transaction, result: "failed", invoice_id: @invoice_23.id)
-    @transaction_24 = create(:transaction, result: "success", invoice_id: @invoice_24.id)
-    @transaction_25 = create(:transaction, result: "failed", invoice_id: @invoice_25.id)
+    @transaction_21 = create(:transaction, result: 1, invoice_id: @invoice_21.id)
+    @transaction_22 = create(:transaction, result: 1, invoice_id: @invoice_22.id)
+    @transaction_23 = create(:transaction, result: 1, invoice_id: @invoice_23.id)
+    @transaction_24 = create(:transaction, result: 0, invoice_id: @invoice_24.id)
+    @transaction_25 = create(:transaction, result: 1, invoice_id: @invoice_25.id)
 
-    ii_21 = create(:invoice_item, invoice_id: @invoice_24.id, status: "packaged")
+    ii_21 = create(:invoice_item, invoice_id: @invoice_24.id, status: 1)
 
     @customer_3 = create(:customer, first_name: "Danny")
     @invoice_31 = create(:invoice, customer_id: @customer_3.id)
@@ -45,11 +45,11 @@ RSpec.describe "Admin Dashboard" do
     @invoice_34 = create(:invoice, customer_id: @customer_3.id)
     @invoice_35 = create(:invoice, customer_id: @customer_3.id)
 
-    @transaction_31 = create(:transaction, result: "failed", invoice_id: @invoice_31.id)
-    @transaction_32 = create(:transaction, result: "success", invoice_id: @invoice_32.id)
-    @transaction_33 = create(:transaction, result: "success", invoice_id: @invoice_33.id)
-    @transaction_34 = create(:transaction, result: "failed", invoice_id: @invoice_34.id)
-    @transaction_35 = create(:transaction, result: "failed", invoice_id: @invoice_35.id)
+    @transaction_31 = create(:transaction, result: 1, invoice_id: @invoice_31.id)
+    @transaction_32 = create(:transaction, result: 0, invoice_id: @invoice_32.id)
+    @transaction_33 = create(:transaction, result: 0, invoice_id: @invoice_33.id)
+    @transaction_34 = create(:transaction, result: 1, invoice_id: @invoice_34.id)
+    @transaction_35 = create(:transaction, result: 1, invoice_id: @invoice_35.id)
 
     @customer_4 = create(:customer, first_name: "Cate")
     @invoice_41 = create(:invoice, customer_id: @customer_4.id)
@@ -58,11 +58,11 @@ RSpec.describe "Admin Dashboard" do
     @invoice_44 = create(:invoice, customer_id: @customer_4.id)
     @invoice_45 = create(:invoice, customer_id: @customer_4.id)
 
-    @transaction_41 = create(:transaction, result: "success", invoice_id: @invoice_41.id)
-    @transaction_42 = create(:transaction, result: "success", invoice_id: @invoice_42.id)
-    @transaction_43 = create(:transaction, result: "success", invoice_id: @invoice_43.id)
-    @transaction_44 = create(:transaction, result: "failed", invoice_id: @invoice_44.id)
-    @transaction_45 = create(:transaction, result: "failed", invoice_id: @invoice_45.id)
+    @transaction_41 = create(:transaction, result: 0, invoice_id: @invoice_41.id)
+    @transaction_42 = create(:transaction, result: 0, invoice_id: @invoice_42.id)
+    @transaction_43 = create(:transaction, result: 0, invoice_id: @invoice_43.id)
+    @transaction_44 = create(:transaction, result: 1, invoice_id: @invoice_44.id)
+    @transaction_45 = create(:transaction, result: 1, invoice_id: @invoice_45.id)
 
     @customer_5 = create(:customer, first_name: "Bob")
     @invoice_51 = create(:invoice, customer_id: @customer_5.id)
@@ -71,11 +71,11 @@ RSpec.describe "Admin Dashboard" do
     @invoice_54 = create(:invoice, customer_id: @customer_5.id)
     @invoice_55 = create(:invoice, customer_id: @customer_5.id)
 
-    @transaction_51 = create(:transaction, result: "success", invoice_id: @invoice_51.id)
-    @transaction_52 = create(:transaction, result: "success", invoice_id: @invoice_52.id)
-    @transaction_53 = create(:transaction, result: "success", invoice_id: @invoice_53.id)
-    @transaction_54 = create(:transaction, result: "success", invoice_id: @invoice_54.id)
-    @transaction_55 = create(:transaction, result: "failed", invoice_id: @invoice_55.id)
+    @transaction_51 = create(:transaction, result: 0, invoice_id: @invoice_51.id)
+    @transaction_52 = create(:transaction, result: 0, invoice_id: @invoice_52.id)
+    @transaction_53 = create(:transaction, result: 0, invoice_id: @invoice_53.id)
+    @transaction_54 = create(:transaction, result: 0, invoice_id: @invoice_54.id)
+    @transaction_55 = create(:transaction, result: 1, invoice_id: @invoice_55.id)
 
     customer_6 = create(:customer)
     customer_7 = create(:customer)
@@ -139,22 +139,27 @@ RSpec.describe "Admin Dashboard" do
         visit admin_index_path
 
         within ".incomplete_invoices" do
+
           expect(page).to have_content("#{@invoice_1.id}")
           expect(page).to have_content("#{@invoice_3.id}")
           expect(page).to have_content("#{@invoice_24.id}")
-          expect(page).to _not have_content("#{@invoice_2.id}")
-          expect(page).to _not have_content("#{@invoice_4.id}")
+          expect(page).to_not have_content("#{@invoice_2.id}")
+          expect(page).to_not have_content("#{@invoice_4.id}")
         end
       end
       it "should display the date formatted like 'Monday, July 18, 2019'" do
         visit admin_index_path
 
-        expect().to have_content()
+        expect(page).to have_content(@invoice_1.created_at.strftime('%A, %B %d, %Y'))
+        expect(page).to have_content(@invoice_3.created_at.strftime('%A, %B %d, %Y'))
+        expect(page).to have_content(@invoice_24.created_at.strftime('%A, %B %d, %Y'))
       end
       it "should link to the admin/show page when the id is clicked" do
         visit admin_index_path
 
-        expect().to have_content()
+        find_link("#{@invoice_1.id}")
+        find_link("#{@invoice_3.id}")
+        find_link("#{@invoice_24.id}")
       end
       it "should list the invoice ids from oldest to newest" do
 
