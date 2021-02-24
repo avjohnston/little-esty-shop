2f293e0c86b091e12e76c840a86e5dd4602ac6d6
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 20:11:31 -0500

    Create controller instance vaiable @incomplete_invoices

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,6 @@ class AdminController < ApplicationController
 
   def index
     @top_customers = Customer.all_successful_transactions_count.order('successful_transactions_count desc').limit(5)
+    @incomplete_invoices = Invoices.all_incomplete_invoices
   end
 end
