c8be3993df1cf5cede7cf3d93c54bbb69c6f5457
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-24 10:18:32 -0500

    Rename method to include by_customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
-  def self.all_successful_transactions_count
+  def self.all_successful_transactions_by_customer_count
     Customer.joins(:transactions).where("transactions.result = ?", 1).select("customers.*, count('transactions') as successful_transactions_count").group(:id)
   end
 end
