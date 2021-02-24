5e230152c6ebc44741277e5f5bb92f0c8af709cd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 21:09:49 -0500

    Add distinct(:id) to ::all_invoices_with_unshipped_items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,6 @@ class Invoice < ApplicationRecord
   belongs_to :customer
 
   def self.all_invoices_with_unshipped_items
-    joins(:invoice_items).where('invoice_items.status = ?', 0)
+    joins(:invoice_items).where('invoice_items.status = ?', 1).distinct(:id)
   end
 end
