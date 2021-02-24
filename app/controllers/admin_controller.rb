90fa4c2783f70d521dd4f9e6589fa73508c47226
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 22:23:34 -0500

    Remove :desc from order to get incomplete invoices listed oldest to newest

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
 
   def index
     @top_customers = Customer.all_successful_transactions_count.order('successful_transactions_count desc').limit(5)
-    @incomplete_invoices = Invoice.all_invoices_with_unshipped_items.order(created_at: :desc)
+    @incomplete_invoices = Invoice.all_invoices_with_unshipped_items.order(:created_at)
   end
 end
