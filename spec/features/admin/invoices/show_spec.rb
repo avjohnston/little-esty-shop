2a549d8cbffddaa2f30b47dae910f2efb51b4db7
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-24 18:22:47 -0500

    Create tests for Admin Invoice Show Page

---
+++ spec/features/admin/invoices/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin invoices show page' do
+  before :each do
+    @invoice_1 = create(:invoice)
+  end
+
+  describe 'as an admin' do
+    describe'displays invoice information' do
+      it 'like id, status, created_at in Day, Month DD, YYYY' do
+        visit admin_invoice_path(@invoice_1)
+
+
+        expect(page).to have_content(@invoice_1.id)
+        expect(page).to have_content(@invoice_1.status)
+        expect(page).to have_content(@invoice_1.created_at.strftime('%A, %B %d, %Y'))
+      end
+    end
+  end
+end
