cb9c3866fbba8c18930bf85546c4a946b9a9f159
Angel Breaux <abreaux@detaso.com>
2021-02-24 22:20:33 -0600

    Create spec tests for enabled and disabled buttons

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Item do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should belong_to :merchant }
   end
+
+  before :each do
+    @merchant = create(:merchant)
+
+    @item = create(:item, merchant_id: @merchant.id, status: :enabled)
+    @item2 = create(:item, merchant_id: @merchant.id, status: :disabled)
+  end
+
+  describe 'class methods' do
+    it 'returns enabled items' do
+      expected = [@item]
+
+      expect(Item.enabled).to eq(expected)
+    end
+
+    it 'returns disabled items' do
+      expected = [@item2]
+
+      expect(Item.disabled).to eq(expected)
+    end
+  end
 end
