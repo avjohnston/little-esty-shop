f347720722875cb3de00aeafad97f049545a25a0
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-25 14:41:16 -0500

    Add tests for invoice item information displaying on invoice show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe 'Admin invoices show page' do
   before :each do
     @customer_1 = create(:customer)
     @invoice_1 = create(:invoice, customer_id: @customer_1.id)
+    @item_1 = create(:item)
+    @item_2 = create(:item)
+    @item_3 = create(:item)
+    @invoice_1.items << @item_1
+    @invoice_1.items << @item_2
+    @invoice_1.items << @item_3
   end
 
   describe 'as an admin' do
@@ -25,6 +31,24 @@ RSpec.describe 'Admin invoices show page' do
         within '.admin_invoice#customer_info' do
           expect(page).to have_content(@customer_1.full_name)
           # expect(page).to have_content(@customer_1.shipping_address)
+        end
+      end
+    end
+
+    describe'displays invoice_items information' do
+      it 'like name, quantity, price, and status' do
+        visit admin_invoice_path(@invoice_1)
+
+        within '.admin_invoice#ii_info' do
+          expect(page).to have_content(@item_1.name)
+          expect(page).to have_content(@item_2.name)
+          expect(page).to have_content(@item_3.name)
+          expect(page).to have_content(@item_1.status)
+          expect(page).to have_content(@item_2.status)
+          expect(page).to have_content(@item_3.status)
+          expect(page).to have_content(@item_1.unit_price)
+          expect(page).to have_content(@item_2.unit_price)
+          expect(page).to have_content(@item_3.unit_price)
         end
       end
     end
