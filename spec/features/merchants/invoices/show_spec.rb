ca2daf9d9657974e53445d16d5b96ff5ebc6d30b
Angel Breaux <abreaux@detaso.com>
2021-02-25 20:55:50 -0600

    Create spec for invoice customer

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -3,16 +3,27 @@ require 'rails_helper'
 RSpec.describe 'As a merchant' do
   before :each do
     @merchant = create(:merchant)
-    @invoice = create(:invoice)
+    @customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+
+    @invoice_1 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_2 = create(:invoice, customer_id: @customer_2.id)
   end
 
   describe 'When I visit my merchants invoice show page(/merchants/merchant_id/invoices/invoice_id)' do
     it 'I see information related to that invoice' do
-      visit merchant_invoice_path(@merchant, @invoice)
+      visit merchant_invoice_path(@merchant, @invoice_1)
 
-      expect(page).to have_content(@invoice.id)
-      expect(page).to have_content(@invoice.status)
-      expect(page).to have_content(@invoice.created_at.strftime('%A, %B %d, %Y'))
+      expect(page).to have_content(@invoice_1.id)
+      expect(page).to have_content(@invoice_1.status)
+      expect(page).to have_content(@invoice_1.created_at.strftime('%A, %B %d, %Y'))
+    end
+
+    it 'I see all of the customer information related to that invoice' do
+      visit merchant_invoice_path(@merchant, @invoice_1)
+
+      expect(page).to have_content(@customer_1.full_name)
+      expect(page).not_to have_content(@customer_2.full_name)
     end
   end
 end
