54753e8518cfd8ce991ae10b506a1cee8cf8d41e
Angel Breaux <abreaux@detaso.com>
2021-02-25 20:33:29 -0600

    Create spec for invoice index page, update controller and view

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,4 +1,9 @@
 class InvoicesController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoices = Invoice.find_from_merchant(@merchant.id)
+  end
+
   def show
     @invoice = Invoice.find(params[:merchant_id])
   end
