725dfdb3e0fe8b599a5bac6876a984a9825832f9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-27 08:51:41 -0500

    Remove unused helper methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -15,12 +15,4 @@ class InvoiceItem < ApplicationRecord
   def self.search_for_quantity(invoiceid, itemid)
     select(:quantity).find_by(invoice_id: invoiceid, item_id: itemid).quantity
   end
-
-  def self.search_for_unit_price(invoiceid, itemid)
-    select(:unit_price).find_by(invoice_id: invoiceid, item_id: itemid).unit_price
-  end
-
-  def self.potential_revenue(invoiceid, itemid)
-    search_for_quantity(invoiceid, itemid) * search_for_unit_price(invoiceid, itemid)
-  end
 end
