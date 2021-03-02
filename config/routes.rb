a2e0c18b77d9db4e12edd26fd10cf9b00741c9c8
Angel Breaux <abreaux@detaso.com>
2021-03-01 23:12:05 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants do
     resources :dashboard, only: :index
-    resources :items
-    resources :invoices
-    resources :invoice_items, only: [:update]
+    resources :items, except: :destroy
+    resources :invoices, only: [:index, :show]
+    resources :invoice_items, only: :update
   end
 
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants
-    resources :invoices
+    resources :merchants, except: :destroy
+    resources :invoices, only: [:index, :show, :update]
   end
 end
