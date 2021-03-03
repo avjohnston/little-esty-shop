062af1a2d964a2111d07b36c2df81d22ee187973
Alex Schwartz <aschwartz@detaso.com>
2021-03-03 09:01:55 -0600

    Update admin merchants to use flash messages and form partial

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -1,26 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'Admin merchants show spec' do
+RSpec.describe 'Admin merchants edit spec' do
   before :each do
-    @merchant1 = create(:merchant)
+    @merchant = create(:merchant)
   end
 
   describe 'as an admin' do
     it "loads with a form populated with merchant's info" do
-      visit edit_admin_merchant_path(@merchant1)
+      visit edit_admin_merchant_path(@merchant)
 
       within('#form') do
-        expect(page.find('#merchant_name').value).to eq(@merchant1.name)
+        expect(page.find('#merchant_name').value).to eq(@merchant.name)
         fill_in('merchant_name', with: 'WPI Records')
         click_on 'submit'
       end
 
-      expect(current_path).to eq(admin_merchant_path(@merchant1))
+      expect(current_path).to eq(admin_merchant_path(@merchant))
 
       expect(page).to have_selector('.flash-message')
       within('#merchant-info') do
         expect(page).to have_content('WPI Records')
       end
+    end
+
+    it 'shows flash message if fields are saved blank' do
+      visit edit_admin_merchant_path(@merchant)
+
+      within('#form') do
+        fill_in('merchant_name', with: '')
+        click_on 'submit'
+      end
+
+      expect(current_path).to eq(edit_admin_merchant_path(@merchant))
+      expect(page).to have_selector('.flash-message')
     end
   end
 end
--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Admin merchants index spec' do
+RSpec.describe 'Admin merchants new spec' do
   describe 'as an admin' do
     it 'loads with a form to create a new merchant' do
       visit new_admin_merchant_path
@@ -15,6 +15,18 @@ RSpec.describe 'Admin merchants index spec' do
       within('#disabled-merchants') do
         expect(page).to have_content('WPI Records')
       end
+    end
+
+    it 'shows flash message if fields are saved blank' do
+      visit new_admin_merchant_path
+
+      within('#form') do
+        fill_in('merchant_name', with: '')
+        click_on 'submit'
+      end
+
+      expect(current_path).to eq(new_admin_merchant_path)
+      expect(page).to have_selector('.flash-message')
     end
   end
 end
