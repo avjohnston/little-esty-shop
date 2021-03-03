4fbc3b9af6a3af4b689bbc3ef442000ba6575ddc
abreaux26 <angelbreaux26@gmail.com>
2021-03-03 14:59:05 -0600

    Fix item best day

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,7 +26,6 @@ class Item < ApplicationRecord
   def best_day
     invoices.joins(:invoice_items)
             .select('invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
-            .where('invoice_items.status = 2')
             .group('invoices.created_at')
             .order('total_revenue desc')
             .first
